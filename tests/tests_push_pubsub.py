c7059148d5d445066b9c92a51bcfe5c53b75dcea
Daniel Cardinal <daniel.cardinal.gcp2@gmail.com>
2023-05-29 16:27:45 +0000

    minor updates to tests_push_pubsub.py

--- tests/tests_push_pubsub.py
+++ tests/tests_push_pubsub.py
@@ -1,3 +1,5 @@
+from google.cloud import functions_v1
+from google.oauth2 import service_account
 from fastavro import parse_schema, writer
 from google.cloud import logging
 import unittest
@@ -10,16 +12,45 @@ FUNCTION_NAME = "process_object" # folder name containing main.py
 CLOUD_FUNCTION_NAME = "data_validation_func"
 
 
+def check_function_execution_status(project_id, function_name):
+    # Load the service account credentials
+    
+    # Initialize the Cloud Functions client
+    client = functions_v1.CloudFunctionsServiceClient()
+    
+    # Construct the parent resource name
+    parent = f'projects/{project_id}/locations/us-central1'
+    
+    # Construct the Cloud Function name
+    function = f'{parent}/functions/{function_name}'
+    
+    # Retrieve the Cloud Function's status
+    response = client.get_function(name=function)
+    
+    # Check the status field to determine if the function is being executed
+    while True:
+        if response.status != functions_v1.FunctionStatus.RUNNING:
+            print("Cloud Function is done executing")
+            break
+            
+        else:
+            print("Cloud Function is currently being executed.")
+
+
+
+
+
 def read_cloud_function_logs(project_id:str, function_name:str):
     # Initialize the Logging client
     client = logging.Client(project=project_id)
-    
+
     # Define the log filter to retrieve Cloud Function logs
     filter_str = f'resource.type="cloud_function" AND resource.labels.function_name="{function_name}"'
     
     # Retrieve the Cloud Function logs
     logs = client.list_entries(filter_=filter_str)
     
+    print(logs)
     # Process and print the log entries
     for log_entry in logs:
         print(log_entry)
@@ -69,12 +100,16 @@ class TestPubSubPushCloudFunction(unittest.TestCase):
         ]
 
     def test_create_new_object_in_bucket(self):
-        file_name = "weather8.avro"
+        file_name = "weather12.avro"
         create_dummy_avro_file_locally(local_path=f'tests/{file_name}', 
                                         schema=self.schema_avro, 
                                         records=self.weather_records)
         
-        copy_file_to_bucket('tests/{file_name}', BUCKET, file_name)
+        copy_file_to_bucket(f'tests/{file_name}', BUCKET, file_name)
 
+
+        #check_function_execution_status(project_id="sacred-truck-387712",
+        #                            function_name=CLOUD_FUNCTION_NAME)
+                                                                        
         read_cloud_function_logs(project_id="sacred-truck-387712",
-                                 function_name=FUNCTION_NAME)
+                                 function_name=CLOUD_FUNCTION_NAME)
